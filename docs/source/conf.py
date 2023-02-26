59a8a6e6eea4149ba50aa2c63fcfd2744f024af3
Shlublu <vincent.poulain@sacem.fr>
2023-02-26 22:21:43 +0100

    Documentation: sphinx test

---
+++ docs/source/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+from datetime import datetime
+
+project = 'awsmate'
+copyright = f'{datetime.now().year}, Vincent Poulain (shlublu)'
+author = 'shlublu'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleaon',
+    'sphinx.ext.numpydoc',
+]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.py': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+master_doc = "index.md"
