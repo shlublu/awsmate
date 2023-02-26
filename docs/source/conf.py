adfe5d71391830f43a102755366c40911f2b29ab
Shlublu <vincent.poulain@sacem.fr>
2023-02-27 00:55:49 +0100

    RTD config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,27 +2,41 @@
 
 import os
 import sys
+
 from datetime import datetime
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath('../../src'))
 
-# -- Project information
+# -- Project information -----------------------------------------------------
 
-project = "awsmate"
+project = 'awsmate'
 copyright = f"{datetime.now().year}, Vincent Poulain (shlublu)"
-author = "shlublu"
+author = 'shlublu'
 
-# -- General configuration
+# The full version, including alpha/beta/rc tags
+release = '0.0.2'
 
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "myst_parser",
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'myst_parser',
 ]
 
-source_suffix = {
-    ".rst": "restructuredtext",
-    ".md": "markdown",
-}
-
 master_doc = "README.md"
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'alabaster'
+
+# -- Extension configuration -------------------------------------------------
+
+# Napoleon settings
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+
+
