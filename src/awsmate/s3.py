60600b23e939fdb07294d3d223ffbd9a87235915
Shlublu <vincent.poulain@sacem.fr>
2023-03-15 01:33:41 +0100

    S3: fixed a naming issue and wrote some documentation

--- src/awsmate/s3.py
+++ src/awsmate/s3.py
@@ -4,15 +4,56 @@ from awsmate.lambdafunction import LambdaEvent, AwsEventSpecificationError
 
 
 class LambdaNotificationEvent(LambdaEvent):
+    """
+    Mapping of the input event received by an AWS Lambda function triggered by AWS S3 Notification.
+    """
+
     KEY_S3 = "s3"
     KEY_RECORDS = "Records"
 
 
-    def __init__(self, eventObject: dict):
-        super().__init__(eventObject)
+    def __init__(self, event_object: dict):
+        """
+        Parameters
+        ----------
+        event_object : dict
+            The parameter ``event`` received by the AWS Lambda function handler.
+
+        Raises
+        ------
+        TypeError
+            If ``event_object`` is not a ``dict``.  
+                    
+        Examples
+        --------
+        >>> def lambda_handler(raw_event, context):
+        >>>     from awsmate.s3 import LambdaNotificationEvent
+        >>>     event = LambdaNotificationEvent(raw_event)                
+        """
+
+        super().__init__(event_object)
 
 
-    def objet_key(self) -> str:
+    def object_key(self) -> str:
+        """
+        Returns the key of the S3 object that is the subject of this notification.
+
+        Returns
+        -------
+        str
+            The key of the S3 object.
+
+        Raises
+        ------
+        awsmate.lambdafunction.AwsEventSpecificationError
+            If the event structure does not allow retrieving this key.         
+
+        Examples
+        --------
+        >>> event.objet_key()
+        'path/to/some/object'
+        """
+        
         try:
             ret = self._event[LambdaNotificationEvent.KEY_RECORDS][0][LambdaNotificationEvent.KEY_S3]["object"]["key"]
     
