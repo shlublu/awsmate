a4fccdaeeded982988b492bdba10494d8c0d2ae8
Shlublu <vincent.poulain@sacem.fr>
2023-03-16 02:22:59 +0100

    Documentation: TOC

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,7 @@ release = '0.0.3'
 extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
     'sphinx_toolbox.github',
     'sphinx_toolbox.sidebar_links',
     'myst_parser'
